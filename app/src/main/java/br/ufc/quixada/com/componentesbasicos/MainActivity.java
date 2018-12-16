16e890b8c53adb9650213856d8b82e654b709dbf
Caio Souza <caiosouza780@gmail.com>
2018-12-16 16:43:37 -0200

    Push inicial

---
+++ app/src/main/java/br/ufc/quixada/com/componentesbasicos/MainActivity.java
@@ -0,0 +1,138 @@
+package br.ufc.quixada.com.componentesbasicos;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton1, toggleButton2;
+    private Button btnDisplay;
+
+    private Button intent;
+
+
+    EditText editText;
+    Spinner spnr;
+    private RadioGroup radioGroup;
+    String[] celebrities = {
+            "Alemanha",
+            "Brail",
+            "Belgica",
+            "Austrália",
+            "Argentina",
+            "Bolivia",
+            "Canadá",
+            "França ",
+            "Holanda"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+
+    public void onClear(View v) {
+        /* Clears all selected radio buttons to default */
+        radioGroup.clearCheck();
+    }
+
+    public void onSubmit(View v) {
+        RadioButton rb = (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+        Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void navegar(View v){
+        Intent intent = new Intent(this, navegar.class);
+        startActivity(intent);
+    }
+
+    public void addListenerOnButton() {
+
+        intent = (Button) findViewById(R.id.intent);
+
+
+        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
+        toggleButton2 = (ToggleButton) findViewById(R.id.toggleButton2);
+        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+        editText = (EditText) findViewById(R.id.editText);
+        spnr = (Spinner) findViewById(R.id.spinner);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.clearCheck();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, celebrities);
+
+        spnr.setAdapter(adapter);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if(null!=rb && checkedId > -1){
+                    Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+        spnr.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                               int arg2, long arg3) {
+
+                        int position = spnr.getSelectedItemPosition();
+                        Toast.makeText(getApplicationContext(),"You have selected "+celebrities[+position],Toast.LENGTH_LONG).show();
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> arg0) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                }
+        );
+
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer result = new StringBuffer();
+                result.append("toggleButton1 : ").append(toggleButton1.getText());
+                result.append("\ntoggleButton2 : ").append(toggleButton2.getText());
+
+                Toast.makeText(MainActivity.this, result.toString(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+
+
+    }
+}
